0daa2356297e01074ba2b406b3c1a71c1bba99a9
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-09-01 22:04:14 -0400

    Update LexLocker.sol

--- general/LexLocker.sol
+++ general/LexLocker.sol
@@ -1,16 +1,10 @@
 /*
-██╗     ███████╗██╗  ██╗                         
-██║     ██╔════╝╚██╗██╔╝                         
-██║     █████╗   ╚███╔╝                          
-██║     ██╔══╝   ██╔██╗                          
-███████╗███████╗██╔╝ ██╗                         
-╚══════╝╚══════╝╚═╝  ╚═╝                         
- ██████╗ ██╗   ██╗██╗██╗     ██████╗             
-██╔════╝ ██║   ██║██║██║     ██╔══██╗            
-██║  ███╗██║   ██║██║██║     ██║  ██║            
-██║   ██║██║   ██║██║██║     ██║  ██║            
-╚██████╔╝╚██████╔╝██║███████╗██████╔╝            
- ╚═════╝  ╚═════╝ ╚═╝╚══════╝╚═════╝             
+██╗     ███████╗██╗  ██╗
+██║     ██╔════╝╚██╗██╔╝
+██║     █████╗   ╚███╔╝ 
+██║     ██╔══╝   ██╔██╗ 
+███████╗███████╗██╔╝ ██╗
+╚══════╝╚══════╝╚═╝  ╚═╝
 ██╗      ██████╗  ██████╗██╗  ██╗███████╗██████╗ 
 ██║     ██╔═══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗
 ██║     ██║   ██║██║     █████╔╝ █████╗  ██████╔╝
@@ -19,25 +13,55 @@
 ╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
 DEAR MSG.SENDER(S):
 
-/ LXGL is a project in beta.
-// Please audit & use at your own risk.
-/// Entry into LXGL shall not create an attorney/client relationship.
-//// Likewise, LXGL should not be construed as legal advice or replacement for professional counsel.
+/ LXL is a project in beta.
+// Please audit and use at your own risk.
+/// Entry into LXL shall not create an attorney/client relationship.
+//// Likewise, LXL should not be construed as legal advice or replacement for professional counsel.
 ///// STEAL THIS C0D3SL4W 
 
-~presented by LexDAO | Raid Guild LLC
+~presented by Open, ESQ || LexDAO LLC
 */
 
 pragma solidity 0.5.17;
 
-contract Context { // describes current contract execution context (metaTX support) / openzeppelin-contracts/blob/master/contracts/GSN/Context.sol
-    function _msgSender() internal view returns (address payable) {
-        return msg.sender;
+interface IERC20 { // brief interface for erc20 token txs
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
+library Address { // helper for address type / openzeppelin-contracts/blob/master/contracts/utils/Address.sol
+    function isContract(address account) internal view returns (bool) {
+        // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
+        // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
+        // for accounts without code, i.e. `keccak256('')`
+        bytes32 codehash;
+        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+        assembly { codehash := extcodehash(account) }
+        return (codehash != accountHash && codehash != 0x0);
+    }
+}
+
+library SafeERC20 { // wrapper around erc20 token txs for non-standard contracts / openzeppelin-contracts/blob/master/contracts/token/ERC20/SafeERC20.sol
+    using Address for address;
+
+    function safeTransfer(IERC20 token, address to, uint256 value) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
     }
 
-    function _msgData() internal view returns (bytes memory) {
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
-        return msg.data;
+    function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+    }
+
+   function _callOptionalReturn(IERC20 token, bytes memory data) private {
+        require(address(token).isContract(), "SafeERC20: call to non-contract");
+
+        (bool success, bytes memory returndata) = address(token).call(data);
+        require(success, "SafeERC20: low-level call failed");
+
+        if (returndata.length > 0) { // return data is optional
+            require(abi.decode(returndata, (bool)), "SafeERC20: erc20 operation did not succeed");
+        }
     }
 }
 
@@ -75,221 +99,182 @@ library SafeMath { // wrapper over solidity arithmetic for unit under/overflow c
     }
 }
 
-library Address { // helper for address type / openzeppelin-contracts/blob/master/contracts/utils/Address.sol
-    function isContract(address account) internal view returns (bool) {
-        // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
-        // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
-        // for accounts without code, i.e. `keccak256('')`
-        bytes32 codehash;
-        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
-        assembly { codehash := extcodehash(account) }
-        return (codehash != accountHash && codehash != 0x0);
+contract Context { // describes current contract execution context (metaTX support) / openzeppelin-contracts/blob/master/contracts/GSN/Context.sol
+    function _msgSender() internal view returns (address payable) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
     }
 }
 
-interface IERC20 { // brief interface for erc20 token txs
-    function transfer(address to, uint256 value) external returns (bool);
-
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
-}
-
-library SafeERC20 { // wrapper around erc20 token txs for non-standard contracts / openzeppelin-contracts/blob/master/contracts/token/ERC20/SafeERC20.sol
-    using Address for address;
-
-    function safeTransfer(IERC20 token, address to, uint256 value) internal {
-        _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
-    }
-
-    function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
-        _callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
-    }
-
-   function _callOptionalReturn(IERC20 token, bytes memory data) private {
-        require(address(token).isContract(), "SafeERC20: call to non-contract");
-
-        (bool success, bytes memory returndata) = address(token).call(data);
-        require(success, "SafeERC20: low-level call failed");
-
-        if (returndata.length > 0) { // return data is optional
-            require(abi.decode(returndata, (bool)), "SafeERC20: erc20 operation did not succeed");
-        }
-    }
-}
-
-interface IWETH { // brief interface for canonical ether token wrapper contract 
-    function deposit() payable external;
-    
-    function transfer(address dst, uint wad) external returns (bool);
-}
-
-contract LexGuildLocker is Context { // splittable digital deal lockers w/ embedded arbitration tailored for guild work
-    using SafeMath for uint256;
+contract LexLocker is Context { // swift arbitration protocol with dispute locker
     using SafeERC20 for IERC20;
+    using SafeMath for uint256;
 
-    /** <$> LXGL <$> **/
-    address public wETH = 0xd0A1E359811322d97991E03f863a0C30C2cF029C; // canonical ether token wrapper contract reference (kovan)
-    uint256 public lockerCount;
-    uint256 public constant MAX_DURATION = 63113904; // 2-year limit on locker 
+    /** <⚖️> LXL <⚖️> **/
+    address public lexDAO;
+    uint256 public disputeCount;
+    uint256 public resolutionRate;
+    uint256 public MAX_DURATION; // time limit on token lockup for dispute resolution - default 63113904 (2-year)
     mapping(uint256 => Locker) public lockers; 
 
     struct Locker {  
-        address client; 
-        address[] provider;
+        address plaintiff; 
+        address defendant;
         address resolver;
         address token;
         uint8 confirmed;
-        uint8 locked;
-        uint256[] batch;
-        uint256 cap;
-        uint256 released;
+        uint8 released;
+        uint256 amount;
         uint256 termination;
-        bytes32 details; 
+        string complaint;
+        string response;
     }
     
-    event RegisterLocker(address indexed client, address[] indexed provider, address indexed resolver, address token, uint256[] batch, uint256 cap, uint256 index, uint256 termination, bytes32 details);	
-    event ConfirmLocker(uint256 indexed index, uint256 indexed sum);  
-    event Release(uint256 indexed index, uint256[] indexed milestone); 
-    event Withdraw(uint256 indexed index, uint256 indexed remainder);
-    event Lock(address indexed sender, uint256 indexed index, bytes32 indexed details);
-    event Resolve(address indexed resolver, uint256 indexed clientAward, uint256[] indexed providerAward, uint256 index, uint256 resolutionFee, bytes32 details); 
+    event RegisterComplaint(address indexed plaintiff, address indexed defendant, address indexed resolver, address token, uint256 amount, uint256 index, uint256 termination, string complaint);	
+    event ConfirmResponse(uint256 indexed index, string response);  
+    event WithdrawDeposit(uint256 indexed index);
+    event ResolveDispute(address indexed resolver, uint256 indexed plaintiffAward, uint256 indexed defendantAward, uint256 resolutionFee, uint256 index, string opinion); 
+    event UpdateComplaint(uint256 indexed index, string complaint);
+    event UpdateResponse(uint256 indexed index, string response);
+    event UpdateLockerSettings(address indexed lexDAO, uint256 indexed MAX_DURATION, uint256 indexed resolutionRate, string details);
+    
+    constructor (address _lexDAO, uint256 _MAX_DURATION, uint256 _resolutionRate) public {
+        lexDAO = _lexDAO;
+        MAX_DURATION = _MAX_DURATION;
+        resolutionRate = _resolutionRate;
+    }
 
     /***************
     LOCKER FUNCTIONS
     ***************/
-    function registerLocker( // register locker for token deposit & client deal confirmation
-        address client,
-        address[] calldata provider,
+    function registerComplaint( // register dispute locker for token deposit & defendant confirmation
+        address plaintiff,
+        address defendant,
         address resolver,
         address token,
-        uint256[] calldata batch, 
-        uint256 cap,
-        uint256 milestones,
+        uint256 amount,
         uint256 termination, // exact termination date in seconds since epoch
-        bytes32 details) external returns (uint256) {
-        uint256 sum;
-        for (uint256 i = 0; i < provider.length; i++) {
-            sum = sum.add(batch[i]);
-        }
-        
-        require(sum.mul(milestones) == cap, "deposit != milestones");
+        string calldata complaint) external returns (uint256) {
         require(termination <= now.add(MAX_DURATION), "duration maxed");
         
-        lockerCount = lockerCount + 1;
-        uint256 index = lockerCount;
+        disputeCount = disputeCount + 1;
+        uint256 index = disputeCount;
         
-        lockers[index] = Locker( 
-            client, 
-            provider,
+        lockers[index] = Locker(
+            plaintiff,
+            defendant,
             resolver,
             token,
             0,
             0,
-            batch,
-            cap,
-            0,
+            amount,
             termination,
-            details);
+            complaint,
+            "");
 
-        emit RegisterLocker(client, provider, resolver, token, batch, cap, index, termination, details); 
+        emit RegisterComplaint(plaintiff, defendant, resolver, token, amount, index, termination, complaint); 
         return index;
     }
     
-    function confirmLocker(uint256 index) payable external { // client confirms deposit of cap & locks in deal
+    function confirmResponse(uint256 index, string calldata response) external { // defendant confirms & locks in split of dispute amount 
         Locker storage locker = lockers[index];
         
         require(locker.confirmed == 0, "confirmed");
-        require(_msgSender() == locker.client, "!client");
+        require(_msgSender() == locker.defendant, "!defendant");
         
-        uint256 sum = locker.cap;
+        locker.response = response;
         
-        if (locker.token == wETH && msg.value > 0) {
-            require(msg.value == sum, "!ETH");
-            IWETH(wETH).deposit();
-            (bool success, ) = wETH.call.value(msg.value)("");
-            require(success, "!transfer");
-            IWETH(wETH).transfer(address(this), msg.value);
-        } else {
-            IERC20(locker.token).safeTransferFrom(msg.sender, address(this), sum);
-        }
+        uint256 split = locker.amount.div(2);
         
-        locker.confirmed = 1;
+        IERC20(locker.token).safeTransferFrom(locker.plaintiff, address(this), split);
+        IERC20(locker.token).safeTransferFrom(_msgSender(), address(this), split);
+
+        locker.confirmed = 1; // true
         
-        emit ConfirmLocker(index, sum); 
+        emit ConfirmResponse(index, response); 
     }
 
-    function release(uint256 index) external { // client transfers locker milestone batch to provider(s) 
-    	Locker storage locker = lockers[index];
-	    
-	require(locker.locked == 0, "locked");
-	require(locker.confirmed == 1, "!confirmed");
-	require(locker.cap > locker.released, "released");
-    	require(_msgSender() == locker.client, "!client"); 
-        
-        uint256[] memory milestone = locker.batch;
-        
-        for (uint256 i = 0; i < locker.provider.length; i++) {
-            IERC20(locker.token).safeTransfer(locker.provider[i], milestone[i]);
-            locker.released = locker.released.add(milestone[i]);
-        }
-
-	emit Release(index, milestone); 
-    }
-    
-    function withdraw(uint256 index) external { // withdraw locker remainder to client if termination time passes & no lock
+    function withdraw(uint256 index) external { // withdraw dispute amount deposit to plaintiff & defendant if termination time passes & no resolution
     	Locker storage locker = lockers[index];
         
-        require(locker.locked == 0, "locked");
         require(locker.confirmed == 1, "!confirmed");
-        require(locker.cap > locker.released, "released");
+        require(locker.released == 0, "released");
         require(now > locker.termination, "!terminated");
         
-        uint256 remainder = locker.cap.sub(locker.released); 
+        uint256 split = locker.amount.div(2);
         
-        IERC20(locker.token).safeTransfer(locker.client, remainder);
+        IERC20(locker.token).safeTransferFrom(address(this), locker.plaintiff, split);
+        IERC20(locker.token).safeTransferFrom(address(this), locker.defendant, split);
         
-        locker.released = locker.released.add(remainder); 
+        locker.released = 1; // true
         
-	emit Withdraw(index, remainder); 
+	emit WithdrawDeposit(index); 
     }
     
     /************
     ADR FUNCTIONS
     ************/
-    function lock(uint256 index, bytes32 details) external { // client or main (0) provider can lock remainder for resolution during locker period / update request details
-        Locker storage locker = lockers[index]; 
-        
-        require(locker.confirmed == 1, "!confirmed");
-        require(locker.cap > locker.released, "released");
-        require(now < locker.termination, "terminated"); 
-        require(_msgSender() == locker.client || _msgSender() == locker.provider[0], "!party"); 
-
-	locker.locked = 1; 
-	    
-	emit Lock(_msgSender(), index, details);
-    }
-    
-    function resolve(uint256 index, uint256 clientAward, uint256[] calldata providerAward, bytes32 details) external { // resolver splits locked deposit remainder between client & provider(s)
+    function updateComplaint(uint256 index, string calldata complaint) external { 
         Locker storage locker = lockers[index];
         
-        uint256 remainder = locker.cap.sub(locker.released); 
-	uint256 resolutionFee = remainder.div(20); // calculates dispute resolution fee (5% of remainder)
+        require(locker.confirmed == 1, "!confirmed");
+        require(now < locker.termination, "terminated");
+        require(_msgSender() == locker.plaintiff, "!plaintiff");
+       
+        locker.complaint = complaint;
+
+        emit UpdateComplaint(index, complaint); 
+    }
+    
+ 
+    function updateResponse(uint256 index, string calldata response) external { 
+        Locker storage locker = lockers[index];
+        
+        require(locker.confirmed == 1, "!confirmed");
+        require(now < locker.termination, "terminated");
+        require(_msgSender() == locker.defendant, "!defendant");
+       
+        locker.response = response;
+
+        emit UpdateResponse(index, response); 
+    }
+    
+    
+    function resolveDispute(uint256 index, uint256 plaintiffAward, uint256 defendantAward, string calldata opinion) external { // resolver splits locked deposit between plaintiff & defendant per opinion
+        Locker storage locker = lockers[index];
+        
+	uint256 resolutionFee = locker.amount.div(resolutionRate); // calculate dispute resolution fee
 	    
-	require(locker.locked == 1, "!locked"); 
-	require(locker.cap > locker.released, "released");
+	require(locker.confirmed == 1, "!confirmed");
+	require(locker.released == 0, "released");
+	require(now < locker.termination, "terminated");
 	require(_msgSender() == locker.resolver, "!resolver");
-	require(_msgSender() != locker.client, "resolver == client");
+	require(_msgSender() != locker.plaintiff && _msgSender() != locker.defendant, "resolver == party");
+	require(plaintiffAward.add(defendantAward) == locker.amount.sub(resolutionFee), "resolution != amount");
 	    
-	for (uint256 i = 0; i < locker.provider.length; i++) {
-            require(_msgSender() != locker.provider[i], "resolver == provider");
-            require(clientAward.add(providerAward[i]) == remainder.sub(resolutionFee), "resolution != remainder");
-            IERC20(locker.token).safeTransfer(locker.provider[i], providerAward[i]);
-        }
-  
-        IERC20(locker.token).safeTransfer(locker.client, clientAward);
+	IERC20(locker.token).safeTransfer(locker.plaintiff, plaintiffAward);
+	IERC20(locker.token).safeTransfer(locker.defendant, defendantAward);
         IERC20(locker.token).safeTransfer(locker.resolver, resolutionFee);
 	    
-	locker.released = locker.released.add(remainder); 
+	locker.released = 1; // true 
 	    
-	emit Resolve(_msgSender(), clientAward, providerAward, index, resolutionFee, details);
+	emit ResolveDispute(_msgSender(), plaintiffAward, defendantAward, index, resolutionFee, opinion);
+    }
+    
+    /**************
+    LEXDAO FUNCTION
+    **************/
+    function updateLockerSettings(address _lexDAO, uint256 _MAX_DURATION, uint256 _resolutionRate, string calldata details) external { 
+        require(_msgSender() == lexDAO, "!lexDAO");
+        
+        lexDAO = _lexDAO;
+        MAX_DURATION = _MAX_DURATION;
+        resolutionRate = _resolutionRate;
+	    
+	emit UpdateLockerSettings(lexDAO, MAX_DURATION, resolutionRate, details);
     }
 }
